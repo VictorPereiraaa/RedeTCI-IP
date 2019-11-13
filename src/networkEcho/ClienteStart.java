eed6ac3ebea297c4a24075c856dcf9193345bad0
victo <victo@DESKTOP-5UDLUNL>
2019-11-13 03:10:07 -0200

    ClienteStart e ServerStart add

---
+++ src/networkEcho/ClienteStart.java
@@ -0,0 +1,8 @@
+package networkEcho;
+
+public class ClienteStart {
+	
+	public static void main(String[] args) throws ClassNotFoundException, InterruptedException {
+		Cliente cliente = new Cliente();
+	}
+}
---
+++ src/networkEcho/ServerStart.java
@@ -0,0 +1,9 @@
+package networkEcho;
+
+public class ServerStart {
+	public static void main(String[] args) {
+		
+		System.out.println(Info.getNomeVersao());
+		Server server = new Server();
+	}
+}
